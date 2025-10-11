cfdd116e838abbff474055398a5437a4c3909854
miguelpinheiro2409 <atomictrl@gmail.com>
2025-10-10 21:04:49 -0300

    Springboot

--- src/main/java/com/divide_ai/backend_divide_ai/DivideAiApplication.java
+++ src/main/java/com/divide_ai/backend_divide_ai/DivideAiApplication.java
@@ -3,10 +3,22 @@ package com.divide_ai.backend_divide_ai;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//Commitar essa parte depois
+/* 
 @SpringBootApplication
+@import({DynamoDbTableCreator.java.example})
+@OpenAPIDefinition(info = @info("DivideAi API", version = "0.1.0", description = "API para repartição de despesa", 
+    repository = @repository(name = "Divide-Ai", url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ") ) )
+*/
+
+
+
+
+
+
 public class DivideAiApplication {
     public static void main(String[] args) {
         SpringApplication.run(DivideAiApplication.class, args);
     }
 }
-//Teste
